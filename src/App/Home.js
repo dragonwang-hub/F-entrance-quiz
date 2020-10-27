de286472403b0e04b3a69241748bb42f55b2e61c
dragonwang-hub <915614879@qq.com>
2020-10-27 23:55:11 +0800

    [dragon] feat: 添加调用方法

--- src/App/Home.js
+++ src/App/Home.js
@@ -1,20 +1,43 @@
 import React, { Component } from 'react';
-import { getGroupStudents } from './action';
+import { getGroupStudents, addStudent } from './action';
 import './Home.css';
 
 class Home extends Component {
+  state = {
+    studentList: [],
+    groupsStudent: {},
+  }
+
   getAll = async () => {
     console.log("分组学员");
     const data = await getGroupStudents();
     console.log(data)
   }
+  addStudent = async () => {
+    console.log("添加学员");
+    const data = await addStudent();
+    console.log(data)
+  }
+
+  getAllStudents = async () => {
+    console.log("打印所有学员");
+    const data = await getAllStudents();
+    console.log(data)
+  }
+
   render() {
     return (
       <div className="home-page-card">
         <p className="title">分组列表</p>
         <p className="groupsBtn">
-          <button onClick={this.getAll}>
+          <button onClick={this.getAll} type="button">
             分组学员
+          </button >
+        </p>
+
+        <p className="addBtn">
+          <button onClick={this.addStudent} type="button">
+            添加学员
           </button >
         </p>
       </div>
